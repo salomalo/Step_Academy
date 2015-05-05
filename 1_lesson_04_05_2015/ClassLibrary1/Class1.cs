80b1c8c98f00787bbddb21cd37b43be41778ae6f
unknown <oksana@a5comp4.step.itstep.rv.ua>
2015-05-05 19:14:57 +0300

    Add files

---
+++ 1_lesson_04_05_2015/ClassLibrary1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public enum PersonMaritalStatus
+    {
+        Merried,
+        Single
+    }
+    [Serializable]
+
+    public class Person
+    {
+        string Name;
+        string LastName;
+        int Age;
+        PersonMaritalStatus MaritsalStatus;
+        public Person(string Name, string Lastname, int Age)
+        {
+            this.Name = Name;
+            this.LastName = Lastname;
+            this.Age = Age;
+            this.MaritsalStatus = PersonMaritalStatus.Single;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Person:\nName: " + Name + "\nLastname: " + LastName + "\nAge: " + Age);
+        }
+    }
+
+    public class Employee : Person
+    {
+        string Position;
+        decimal Salary;
+        public Employee(string Name, string Lastname, int Age, string Position, decimal Salary) :
+            base(Name, Lastname, Age)
+        {
+            this.Position = Position;
+            this.Salary = Salary;
+        }
+    }
+
+}
---
+++ 1_lesson_04_05_2015/waitforexit/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace waitforexit
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Process myPr = new Process();
+            //myPr.StartInfo.FileName = "calc.exe";
+            //myPr.Start();
+            //Console.WriteLine("Open process - " + myPr.ProcessName);
+            //myPr.WaitForExit();
+            //Console.WriteLine("ExitCode - " + myPr.ExitCode);
+
+            //Console.WriteLine("Current process - " + Process.GetCurrentProcess().ProcessName);
+            
+            
+            //////////////////////////////////////////////////////////////////////////////////////////
+
+
+            //Process[] _process = Process.GetProcesses();
+
+            ////foreach (Process p in _process)
+            ////{
+            ////   Console.WriteLine(p.ProcessName + " " + p.Id);
+            ////}
+
+            //try
+            //{
+            //    foreach (Process p in _process)
+            //    {
+            //        if(p.ProcessName=="calc")
+            //          p.Kill();
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //  //  Console.WriteLine(e.Message);
+            //}
+
+            //Console.WriteLine("------------------------");
+            //foreach (Process p in _process)
+            //{
+            //    Console.WriteLine(p.ProcessName + " " + p.Id);
+            //}
+
+
+
+
+            ////////////////////////////there is not nececery to recompile project
+
+            Assembly asym = Assembly.Load(AssemblyName.GetAssemblyName("ClassLibrary1.dll"));
+            Module mod = asym.GetModule("ClassLibrary1.dll");
+
+            foreach(Type t in mod.GetTypes())
+                Console.WriteLine(t.FullName);
+
+            Type Person = mod.GetType("ClassLibrary1.Person") as Type;
+
+            object person = Activator.CreateInstance(Person, new object [] {"name","surname",23});
+
+            Person.GetMethod("Print").Invoke(person, null); //GetMethod() - take the information about mathod, Invoke() - execute method with our informations
+
+
+
+
+
+        }
+
+    }
+}
