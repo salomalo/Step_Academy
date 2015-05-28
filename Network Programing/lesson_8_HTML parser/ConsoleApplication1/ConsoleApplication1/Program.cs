1af1ecab1775c594e995257c05baa2d7141e1403
unknown <oksana@a5comp4.step.itstep.rv.ua>
2015-05-28 21:47:19 +0300

    +

---
+++ Network Programing/lesson_8_HTML parser/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart thst = new ThreadStart(Scanner);
+            Thread th = new Thread(thst);
+            th.Start();
+        }
+
+        public static void Scanner()
+        {
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            List<int> portList = new List<int>();
+            for (int port = 0; port < 10000; port++)
+            {
+                try
+                {
+                    TcpListener server = new TcpListener(ipAddr, port);
+                    server.Start();
+                    server.Stop();
+                }
+                catch (Exception x)
+                {
+                    portList.Add(port);
+                    //Console.WriteLine("err");
+                }
+            }
+
+            foreach (int i in portList)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+
+    }
+}
---
+++ Network Programing/lesson_8_HTML parser/ConsoleApplication1/seraliz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace seraliz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument html = web.Load("http://kurs.com.ua/ua/rovno");
+           
+            //Console.WriteLine(html.DocumentNode.Descendants("div").Count());
+
+
+            var block = html.DocumentNode.SelectSingleNode("//*[@id=\"content_center\"]/div[1]");
+            
+
+            Console.WriteLine(block.ChildNodes.Count());
+
+
+
+   
+        }
+
+    }
+
+
+}
