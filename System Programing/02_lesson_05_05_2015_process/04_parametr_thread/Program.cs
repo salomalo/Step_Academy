f9713ccc425922122eaa269c1e978394f606960d
Oksana Melnyk <okssanamel@gmail.com>
2015-06-15 17:57:28 +0300

    update

---
+++ System Programing/02_lesson_05_05_2015_process/04_parametr_thread/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _04_parametr_thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParameterizedThreadStart pts = new ParameterizedThreadStart(Method);
+            Thread t = new Thread(pts);
+            t.Start((object)"2");
+
+
+        }
+
+
+        static void Method(object str)
+        {
+
+            string s = (string)str;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("{0} --- {1}",i.ToString(),s);
+            }
+        
+        }
+
+
+    }
+}
