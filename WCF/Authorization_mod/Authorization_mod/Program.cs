a3a0f7bb168dbdd8c1f7423788c21dfcda739259
unknown <oksana@a5comp4.step.itstep.rv.ua>
2015-06-15 22:01:07 +0300

    +Authorize module

---
+++ WCF/Authorization_mod/Authorization_mod/Program.cs
@@ -0,0 +1,135 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// database for module
+//create database Author_Mod
+//go
+
+//use Author_Mod
+
+//create table [Users]
+//(
+//Id int identity primary key
+//,Name nvarchar (512) not NULL
+//,Surname nvarchar (512) not NULL
+//,LoginName nvarchar (512) not NULL
+//,Pasword nvarchar (512) not NULL
+//,Token nvarchar (512) 
+//,ExpDate DATETIME
+//)
+
+//INSERT INTO [Users] (Name, Surname, LoginName, Pasword)
+//VALUES
+//('Roman','Velihiy','romaska','1234test')
+//,('Lesya','Gook','les','les123')
+
+//select * from [Users]
+
+
+namespace Authorization_mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ServiceHost sh = new ServiceHost(typeof(GetUserInfo), new Uri("http://localhost/GetUserInfo/"));
+            //sh.Open();
+            //Console.WriteLine("enter");
+            //Console.ReadLine();
+
+            GetUserInfo tmp = new GetUserInfo();
+            tmp.Authorize("romaska", "1234test");
+            tmp.GetInfo("toc");
+        }
+    }
+
+
+    public class User
+    {
+        public int Name;
+        public string Name;
+        public string Surname;
+        public string LoginName;
+        public string Pasword;
+        public string Token;
+        public DateTime ExpDate;
+
+    } // сереалізувати юзера в клас
+
+
+
+    [ServiceContract]
+    public interface IGetUserInfo
+    {
+        [OperationContract]
+        string GetInfo(string token);
+        [OperationContract]
+        string Authorize(string LoginName, string Pasword);
+    }
+
+
+    public class GetUserInfo : IGetUserInfo
+    {
+        public string GetInfo(string token)
+        {
+            using (var ctx = new Author_ModEntities1())
+            {
+                var res = from e in ctx.Users
+                          where e.Token == token
+                          select e;
+                if (res.FirstOrDefault() != null)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(res.FirstOrDefault()));
+                        return JsonConvert.SerializeObject(res.FirstOrDefault());
+                }
+
+            }
+
+            return null;
+        } // GetInfo
+
+
+        public void Token_Generator()
+        {
+
+        } // token creator
+
+
+        public string Authorize(string LoginName, string Pasword)
+        {
+            using (var ctx = new Author_ModEntities1())
+            {
+                var res = from e in ctx.Users
+                          where e.LoginName == LoginName && e.Pasword == Pasword
+                          select e;
+
+                if (res.FirstOrDefault() != null)
+                {
+                    if (res.FirstOrDefault().ExpDate >= DateTime.Today)
+                    {
+                        return JsonConvert.SerializeObject(res.FirstOrDefault());
+                    }
+                    else
+                    {
+                        res.FirstOrDefault().ExpDate = DateTime.Now;
+                        res.FirstOrDefault().Token = "toc"; // Token_Generator()
+                        ctx.SaveChanges();
+                        return JsonConvert.SerializeObject(res.FirstOrDefault());
+                    }
+                }
+                return null;
+            }
+
+
+
+        } // Authorize
+
+    }
+
+}
---
+++ WCF/Authorization_mod/UICLient/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace UICLient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usLogin = new System.Windows.Forms.TextBox();
+            this.usPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAuthorize = new System.Windows.Forms.Button();
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // usLogin
+            // 
+            this.usLogin.Location = new System.Drawing.Point(24, 27);
+            this.usLogin.Name = "usLogin";
+            this.usLogin.Size = new System.Drawing.Size(100, 20);
+            this.usLogin.TabIndex = 0;
+            // 
+            // usPass
+            // 
+            this.usPass.Location = new System.Drawing.Point(160, 27);
+            this.usPass.Name = "usPass";
+            this.usPass.Size = new System.Drawing.Size(100, 20);
+            this.usPass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pass";
+            // 
+            // btnAuthorize
+            // 
+            this.btnAuthorize.Location = new System.Drawing.Point(98, 66);
+            this.btnAuthorize.Name = "btnAuthorize";
+            this.btnAuthorize.Size = new System.Drawing.Size(75, 23);
+            this.btnAuthorize.TabIndex = 4;
+            this.btnAuthorize.Text = "Authorize";
+            this.btnAuthorize.UseVisualStyleBackColor = true;
+            this.btnAuthorize.Click += new System.EventHandler(this.btnAuthorize_Click);
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Location = new System.Drawing.Point(98, 119);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnGetInfo.TabIndex = 5;
+            this.btnGetInfo.Text = "GetInfo";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 160);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 90);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGetInfo);
+            this.Controls.Add(this.btnAuthorize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usPass);
+            this.Controls.Add(this.usLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usLogin;
+        private System.Windows.Forms.TextBox usPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAuthorize;
+        private System.Windows.Forms.Button btnGetInfo;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
