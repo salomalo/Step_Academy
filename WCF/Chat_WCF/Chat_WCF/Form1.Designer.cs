0c60e572e30bdff4f18831759d8449dd6a85dccc
Oksana Melnyk <okssanamel@gmail.com>
2015-06-22 18:15:06 +0300

    +chat_WCF

---
+++ WCF/Chat_WCF/Chat_WCF/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Chat_WCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxRemoteIP = new System.Windows.Forms.TextBox();
+            this.tbxRemotePort = new System.Windows.Forms.TextBox();
+            this.tbxLocalPort = new System.Windows.Forms.TextBox();
+            this.tbxClientNick = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxChatWindow = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbxEnterMessage = new System.Windows.Forms.TextBox();
+            this.btnInitial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxRemoteIP
+            // 
+            this.tbxRemoteIP.Location = new System.Drawing.Point(302, 12);
+            this.tbxRemoteIP.Name = "tbxRemoteIP";
+            this.tbxRemoteIP.Size = new System.Drawing.Size(100, 20);
+            this.tbxRemoteIP.TabIndex = 0;
+            // 
+            // tbxRemotePort
+            // 
+            this.tbxRemotePort.Location = new System.Drawing.Point(302, 52);
+            this.tbxRemotePort.Name = "tbxRemotePort";
+            this.tbxRemotePort.Size = new System.Drawing.Size(100, 20);
+            this.tbxRemotePort.TabIndex = 1;
+            // 
+            // tbxLocalPort
+            // 
+            this.tbxLocalPort.Location = new System.Drawing.Point(67, 52);
+            this.tbxLocalPort.Name = "tbxLocalPort";
+            this.tbxLocalPort.Size = new System.Drawing.Size(100, 20);
+            this.tbxLocalPort.TabIndex = 2;
+            // 
+            // tbxClientNick
+            // 
+            this.tbxClientNick.Location = new System.Drawing.Point(67, 12);
+            this.tbxClientNick.Name = "tbxClientNick";
+            this.tbxClientNick.Size = new System.Drawing.Size(100, 20);
+            this.tbxClientNick.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "RemoteIP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RemotePort";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ClientNick";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "LocalPort";
+            // 
+            // tbxChatWindow
+            // 
+            this.tbxChatWindow.Location = new System.Drawing.Point(13, 105);
+            this.tbxChatWindow.Multiline = true;
+            this.tbxChatWindow.Name = "tbxChatWindow";
+            this.tbxChatWindow.Size = new System.Drawing.Size(389, 237);
+            this.tbxChatWindow.TabIndex = 8;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(314, 374);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbxEnterMessage
+            // 
+            this.tbxEnterMessage.Location = new System.Drawing.Point(13, 374);
+            this.tbxEnterMessage.Name = "tbxEnterMessage";
+            this.tbxEnterMessage.Size = new System.Drawing.Size(295, 20);
+            this.tbxEnterMessage.TabIndex = 10;
+            // 
+            // btnInitial
+            // 
+            this.btnInitial.Location = new System.Drawing.Point(176, 76);
+            this.btnInitial.Name = "btnInitial";
+            this.btnInitial.Size = new System.Drawing.Size(75, 23);
+            this.btnInitial.TabIndex = 11;
+            this.btnInitial.Text = "Initial";
+            this.btnInitial.UseVisualStyleBackColor = true;
+            this.btnInitial.Click += new System.EventHandler(this.btnInitial_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 420);
+            this.Controls.Add(this.btnInitial);
+            this.Controls.Add(this.tbxEnterMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbxChatWindow);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxClientNick);
+            this.Controls.Add(this.tbxLocalPort);
+            this.Controls.Add(this.tbxRemotePort);
+            this.Controls.Add(this.tbxRemoteIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxRemoteIP;
+        private System.Windows.Forms.TextBox tbxRemotePort;
+        private System.Windows.Forms.TextBox tbxLocalPort;
+        private System.Windows.Forms.TextBox tbxClientNick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxChatWindow;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbxEnterMessage;
+        private System.Windows.Forms.Button btnInitial;
+    }
+}
+
---
+++ WCF/Chat_WCF/Chat_WCF/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat_WCF
+{
+    public partial class Form1 : Form
+    {
+        static int RemotePort;
+        static int LocalPort;
+        static IPAddress RemoteIPAddr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Initial()
+        {
+            try
+            {
+                RemoteIPAddr = IPAddress.Parse(tbxRemoteIP.Text);
+                RemotePort = Convert.ToInt32(tbxRemotePort.Text);
+                LocalPort = Convert.ToInt32(tbxLocalPort.Text);
+                //отдельный поток для чтения в методе ThreadFuncReceive
+                //этот метод вызывает метод Receive() класса UdpClient,
+                //который блокирует текущий поток, поэтому необходим отдельный
+                //поток
+                Thread thread = new Thread(
+                       new ThreadStart(ThreadFuncReceive)
+                );
+                //создание фонового потока
+                thread.IsBackground = true;
+                //запуск потока
+                thread.Start();
+            }
+            catch (FormatException formExc)
+            {
+                MessageBox.Show("Преобразование невозможно :" + formExc);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Ошибка:" + exc.Message);
+            }
+        }
+
+        public void ThreadFuncReceive()
+        {
+            try
+            {
+                while (true)
+                {
+                    //подключение к локальному хосту
+                    UdpClient uClient = new UdpClient(LocalPort);
+                    IPEndPoint ipEnd = null;
+                    //получание дейтаграммы
+                    byte[] responce = uClient.Receive(ref ipEnd);
+                    //преобразование в строку
+                    string strResult = Encoding.Unicode.GetString(responce) + Environment.NewLine;
+                    
+                    //вывод на экран
+                    SetText(strResult);
+                    //tbxChatWindow.Text += strResult;
+                   
+                    uClient.Close();
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show("Ошибка сокета: :" + sockEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка:" + ex.Message);
+            }
+        }
+
+        private void btnInitial_Click(object sender, EventArgs e)
+        {
+            Initial();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendData(tbxEnterMessage.Text);
+        }
+
+        public void SendData(string datagramm)
+        {
+            SetText(tbxClientNick.Text + ": " + datagramm);
+            UdpClient uClient = new UdpClient();
+            //подключение к удаленному хосту
+            IPEndPoint ipEnd = new IPEndPoint(RemoteIPAddr, RemotePort);
+            try
+            {
+                byte[] bytes = Encoding.Unicode.GetBytes(tbxClientNick.Text + ": " + datagramm + Environment.NewLine);
+                uClient.Send(bytes, bytes.Length, ipEnd);
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show("Ошибка сокета: :" + sockEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка:" + ex.Message);
+            }
+            finally
+            {
+                //закрытие экземпляра класса UdpClient
+                uClient.Close();
+            }
+        } // SendData
+
+
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.tbxChatWindow.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.tbxChatWindow.Text += text + Environment.NewLine;
+            }
+        }
+
+
+
+
+
+    }
+}
